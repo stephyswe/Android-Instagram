64c91e10f9a748927351483838137aa1858ce586
stephyswe <stephaniiehallberg@gmail.com>
2018-09-12 19:25:24 +0200

    Account Settings Layout

---
+++ app/src/main/java/com/stephanie/instragramclone/Profile/AccountSettingsActivity.java
@@ -0,0 +1,19 @@
+package com.stephanie.instragramclone.Profile;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.stephanie.instragramclone.R;
+
+public class AccountSettingsActivity extends AppCompatActivity{
+
+    private static final String TAG = "AccountSettingsActivity";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_settings);
+        Log.d(TAG, "onCreate: started");
+    }
+}
--- app/src/main/java/com/stephanie/instragramclone/Profile/ProfileActivity.java
+++ app/src/main/java/com/stephanie/instragramclone/Profile/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.stephanie.instragramclone.Profile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.stephanie.instragramclone.R;
@@ -29,26 +32,28 @@ public class ProfileActivity extends AppCompatActivity {
         setupToolbar();
     }
 
+    /**
+     * Responsible for setting up the profile toolbar
+     */
     private void setupToolbar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolbar);
         setSupportActionBar(toolbar);
-        
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+
+        ImageView profileMenu = (ImageView) findViewById(R.id.profileMenu);
+        profileMenu.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                Log.d(TAG, "onMenuItemClick: Clicked menu item: " + item);
-
-                switch (item.getItemId()){
-                    case R.id.profileMenu:
-                        Log.d(TAG, "onMenuItemClick: Navigation to Profile Prefs");
-                }
-
-                    return false;
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to account settings");
+                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
+                startActivity(intent);
             }
         });
     }
 
-    //* Bottom Nav View setup *
+
+    /**
+     * Bottom Navigation Setup
+     */
     private void setupBottomNavigationView() {
         Log.d(TAG, "setupBottomNavigationView: setup up BottomNavView");
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
@@ -60,10 +65,5 @@ public class ProfileActivity extends AppCompatActivity {
         menuItem.setChecked(true);
     }
 
-    // Creates the Items in Profile View
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.profile_menu, menu);
-        return true;
-    }
+
 }
