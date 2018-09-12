ae13730f45415ed452e7a881b22d820a40fe2bb3
stephyswe <stephaniiehallberg@gmail.com>
2018-09-12 19:36:41 +0200

    Account Settings Navigation

--- app/src/main/java/com/stephanie/instragramclone/Profile/AccountSettingsActivity.java
+++ app/src/main/java/com/stephanie/instragramclone/Profile/AccountSettingsActivity.java
@@ -1,19 +1,56 @@
 package com.stephanie.instragramclone.Profile;
 
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.stephanie.instragramclone.R;
+
+import java.util.ArrayList;
 
 public class AccountSettingsActivity extends AppCompatActivity{
 
     private static final String TAG = "AccountSettingsActivity";
+
+    private Context mContext;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
+        mContext = AccountSettingsActivity.this;
         Log.d(TAG, "onCreate: started");
+
+        setupSettingsList();
+
+        //setup back arrow to get back to "ProfileActivity"
+        ImageView backArrow = (ImageView) findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating back to profile page");
+                finish();
+            }
+        });
+    }
+
+    private void setupSettingsList() {
+        Log.d(TAG, "setupSettingsList: init 'Account Settings' List");
+        ListView listView = (ListView) findViewById(R.id.lvAccountSettings);
+
+        ArrayList<String> options = new ArrayList<>();
+        options.add(getString(R.string.edit_profile));
+        options.add(getString(R.string.sign_out));
+
+        ArrayAdapter adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, options);
+        listView.setAdapter(adapter);
+
     }
 }
