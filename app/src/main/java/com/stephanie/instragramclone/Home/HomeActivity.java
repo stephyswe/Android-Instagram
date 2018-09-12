a41543dab52ac0fdfeb35bf6339e5610c6b33c17
Stephanie H <stephaniiehallberg@gmail.com>
2018-09-12 17:30:11 +0200

    SectionsPagerAdapter Tabs

--- app/src/main/java/com/stephanie/instragramclone/Home/HomeActivity.java
+++ app/src/main/java/com/stephanie/instragramclone/Home/HomeActivity.java
@@ -2,6 +2,8 @@ package com.stephanie.instragramclone.Home;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -12,6 +14,8 @@ import com.stephanie.instragramclone.R;
 import com.stephanie.instragramclone.Utils.BottomNavigationViewHelper;
 
 public class HomeActivity extends AppCompatActivity {
+
+    // Stopped at 10:22 in video. part 6.
 
     private static final String TAG = "HomeActivity";
     public static final int ACTIVITY_NUM = 0;
@@ -24,6 +28,29 @@ public class HomeActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: starting. ");
 
         setupBottomNavigationView();
+        setupViewPager();
+    }
+
+    /**
+     * Adds Top Menu (Camera, Logo, Messages) in HomeScreen
+     */
+    private void setupViewPager() {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        adapter.AddFragment(new CameraFragment());
+        adapter.AddFragment(new HomeFragment());
+        adapter.AddFragment(new MessagesFragment());
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_instagram);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
+
+
     }
 
     //* Bottom Nav View setup *
