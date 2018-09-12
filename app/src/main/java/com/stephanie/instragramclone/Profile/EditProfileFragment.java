ce79d70d191f83374153e0815f01fef56a097cc3
stephyswe <stephaniiehallberg@gmail.com>
2018-09-12 21:32:59 +0200

    Universal Image Loader Config

--- app/src/main/java/com/stephanie/instragramclone/Profile/EditProfileFragment.java
+++ app/src/main/java/com/stephanie/instragramclone/Profile/EditProfileFragment.java
@@ -3,21 +3,43 @@ package com.stephanie.instragramclone.Profile;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.stephanie.instragramclone.R;
+import com.stephanie.instragramclone.Utils.UniversalImageLoader;
 
 public class EditProfileFragment extends Fragment {
 
     private static final String TAG = "EditProfileFragment";
 
+    private ImageView mProfilePhoto;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        mProfilePhoto = (ImageView) view.findViewById(R.id.profile_photo);
+
+        initImageLoader();
+
+        setProfileImage();
 
         return view;
+    }
+
+    private void initImageLoader() {
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(getActivity());
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    private void setProfileImage() {
+        Log.d(TAG, "setProfileImage: setting profile image");
+        String imgURL = "cdn.wccftech.com/wp-content/uploads/2017/08/download-android-8.jpg";
+        UniversalImageLoader.setImage(imgURL, mProfilePhoto, null, "https://");
     }
 }
