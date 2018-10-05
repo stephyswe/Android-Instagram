56ca0c7803af14b1e81abf7c8b4d743530016c39
Stephanie H <stephaniiehallberg@gmail.com>
2018-10-05 11:23:28 +0200

    Phone Directories

--- app/src/main/java/com/stephanie/instagramclone/Share/GalleryFragment.java
+++ app/src/main/java/com/stephanie/instagramclone/Share/GalleryFragment.java
@@ -1,5 +1,6 @@
 package com.stephanie.instagramclone.Share;
 
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,6 +8,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -14,6 +17,10 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.stephanie.instagramclone.R;
+import com.stephanie.instagramclone.Utils.FilePaths;
+import com.stephanie.instagramclone.Utils.FileSearch;
+
+import java.util.ArrayList;
 
 public class GalleryFragment extends Fragment {
 
@@ -26,6 +33,7 @@ public class GalleryFragment extends Fragment {
     private Spinner directorySpinner;
 
     //vars
+    private ArrayList<String> directories;
 
 
     @Nullable
@@ -37,6 +45,7 @@ public class GalleryFragment extends Fragment {
         directorySpinner = (Spinner) view.findViewById(R.id.spinnerDirectory);
         mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
         mProgressBar.setVisibility(View.GONE);
+        directories = new ArrayList<>();
 
         Log.d(TAG, "onCreateView: started.");
 
@@ -55,10 +64,41 @@ public class GalleryFragment extends Fragment {
             public void onClick(View v) {
                 Log.d(TAG, "onClick: navigating to the final share screen");
 
-
             }
         });
 
+        init();
+
         return view;
+    }
+
+    private void init() {
+        FilePaths filePaths = new FilePaths();
+
+        // check for other folders inside "/storage/emulated/0/pictures"
+        if (FileSearch.getDirectoryPaths(filePaths.PICTURES) != null) {
+            directories = FileSearch.getDirectoryPaths(filePaths.PICTURES);
+        }
+
+        directories.add(filePaths.CAMERA);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_dropdown_item, directories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        directorySpinner.setAdapter(adapter);
+
+        directorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemSelected: selected: " + directories.get(position));
+
+                // setup our image for the directory chosen
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
